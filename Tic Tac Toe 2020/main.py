213b1f787c53c5b754c2c192b633dd405437b935
ferran <fbarneda@gmail.com>
2020-03-28 02:33:26 +0000

    commit

--- Tic Tac Toe 2020/main.py
+++ Tic Tac Toe 2020/main.py
@@ -8,17 +8,17 @@ def clear_output():
 
 
 def print_the_board(board):
-    print(f"\n               |                 |             ")
+    print("\n               |                 |             ")
     print(f"      {board[6]}        |        {board[7]}        |        {board[8]}    ")
-    print(f"               |                 |             ")
-    print(f"-" * 49)
-    print(f"               |                 |             ")
+    print("               |                 |             ")
+    print("-" * 49)
+    print("               |                 |             ")
     print(f"      {board[3]}        |        {board[4]}        |        {board[5]}    ")
-    print(f"               |                 |             ")
-    print(f"-" * 49)
-    print(f"               |                 |             ")
+    print("               |                 |             ")
+    print("-" * 49)
+    print("               |                 |             ")
     print(f"      {board[0]}        |        {board[1]}        |        {board[2]}    ")
-    print(f"               |                 |             ")
+    print("               |                 |             ")
 
 
 def select_marker():
